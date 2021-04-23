97ab100ab2b8e88aea6792d024a7bb5353b432ee
Yusufaky <marcosta71999@gmail.com>
2021-04-23 15:27:04 +0100

    Api Ponto GetAll

---
+++ app/src/main/java/com/example/android/notas/api/Pontos.kt
@@ -0,0 +1,12 @@
+package com.example.android.notas.api
+
+data class Pontos(
+
+        val id: Int,
+        val latitude: String,
+        val longitude: String,
+        val nome: String,
+        val foto: String,
+        val descricao: String,
+        val id_user: Int,
+)
