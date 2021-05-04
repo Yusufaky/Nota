8767dafc5355eded239f37277cc33f1598a6ce83
Yusufaky <marcosta71999@gmail.com>
2021-05-04 17:43:57 +0100

    Sensores a dar

--- app/src/main/java/com/example/android/notas/MapsActivity.kt
+++ app/src/main/java/com/example/android/notas/MapsActivity.kt
@@ -74,6 +74,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
