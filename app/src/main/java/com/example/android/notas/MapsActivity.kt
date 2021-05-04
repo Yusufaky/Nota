a3ef140ff6656b4e18c9b41fa91fa83a5e7ea87a
Yusufaky <marcosta71999@gmail.com>
2021-05-04 18:19:03 +0100

    ADD pontos a dar

--- app/src/main/java/com/example/android/notas/MapsActivity.kt
+++ app/src/main/java/com/example/android/notas/MapsActivity.kt
@@ -72,6 +72,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
